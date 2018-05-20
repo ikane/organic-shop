879ecca154d888b57b6e46520eb3807bdd6ea2d3
Ibrahima KANE <ikane@mbp-de-ibrahima.home>
2018-05-21 00:39:43 +0200

    add menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ManageOrdersComponent } from './admin/manage-orders/manage-orders.component';
+import { ManageProductsComponent } from './admin/manage-products/manage-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    OrdersComponent,
+    ManageOrdersComponent,
+    ManageProductsComponent
   ],
   imports: [
     BrowserModule,
